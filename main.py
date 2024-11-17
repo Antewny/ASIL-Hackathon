f124c10ac9ef716223e022b92396276bf10f423c
Julia-Cherevko <75038541+Julia-Cherevko@users.noreply.github.com>
2024-11-16 17:40:36 -0800

    update

--- main.py
+++ main.py
@@ -2,23 +2,50 @@
 # profile
 # registration 
 
-from flask import Flask, render_template
+# from flask import Flask, render_template
+# app = Flask(__name__)
+
+# @app.route("/")
+# def index():
+#     return render_template("index.html")
+
+# @app.route("/registration")
+# def registration():
+#     return render_template("registration.html")
+
+# @app.route("/profile")
+# def profile():
+#     return render_template("profile.html")
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+    
+
+from flask import Flask, render_template, redirect, url_for, request, session
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def index():
-    return render_template("index.html")
+    user_logged_in = True; 
+    return render_template('index.html', user_logged_in=user_logged_in)
+
+@app.route("/profile")
+def profile():
+    # username = "Iuliana Cherevko"
+    # days_kept_up = 10
+    # creature_name = "Fido"
+    # creature_level = 5
+    return render_template("profile.html")#, username=username, days_kept_up=days_kept_up, creature_name=creature_name, creature_level=creature_level)
 
 @app.route("/registration")
 def registration():
     return render_template("registration.html")
 
-@app.route("/profile")
-def profile():
-    return render_template("profile.html")
+@app.route('/sign_in', methods=['POST'])
+def sign_in():
+    username = request.form['username']
+    password = request.form['password']
+    #return something
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-       
